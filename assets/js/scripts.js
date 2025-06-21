d5069f48a958c566732feabc19a94387e67035a6
luizfelipeflorian <luiz.felipe.florian.silva12@gmail.com>
2025-06-21 19:18:08 -0400

    estilo: atualizar o esquema de cores e adicionar logotipos e favicons específicos do tema; incluir GIF de carregamento

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,11 +2,26 @@ const toggleTheme = document.getElementById("toggleTheme");
 const rootHtml = document.documentElement
 const accordionHeaders = document.querySelectorAll(".accordion__header");
 const menuLinks = document.querySelectorAll(".menu__link");
+const brand__logo = document.querySelector("#logo-lf");
+const favicon = document.getElementById("favicon");
 
-function changeTheme(){
+const logoDark = "assets/images/logo-lf-dark.png"; // Caminho para a imagem do tema escuro
+const logoLight = "assets/images/logo-lf-light.png"; // Caminho para a imagem do tema claro
+const faviconDark = "favicon_io/favicon-dark.ico"; // Caminho para o favicon do tema escuro
+const faviconLight = "favicon_io/favicon-light.ico"; // Caminho para o favicon do tema claro
+
+function changeTheme() {
   const currentTheme = rootHtml.getAttribute("data-theme");
 
-  currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark")
+  if (currentTheme === "dark") {
+    rootHtml.setAttribute("data-theme", "light");
+    brand__logo.src = logoLight; // Troca para a imagem do tema claro
+    favicon.href = faviconLight; // Troca para o favicon do tema claro
+  } else {
+    rootHtml.setAttribute("data-theme", "dark");
+    brand__logo.src = logoDark; // Troca para a imagem do tema escuro
+    favicon.href = faviconDark; // Troca para o favicon do tema escuro
+  }
 
   toggleTheme.classList.toggle("bi-sun")
   toggleTheme.classList.toggle("bi-moon-stars")
